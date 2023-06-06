d89499418e4cf4cdd7703cc6bd8d0ff38c52dc52
Edasgh <edas25564@gmail.com>
2023-06-06 12:39:13 +0530

    "Operator mistake corrected"

--- JS Assignment Projects/JS-Error-handling-&-Prototypes-Assignment/7. Password Checker/index.js
+++ JS Assignment Projects/JS-Error-handling-&-Prototypes-Assignment/7. Password Checker/index.js
@@ -1,33 +1,38 @@
 //User class
 
-const uppercaseArr="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const uppercaseArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-class User{
-    constructor(username,password){
-        this.username=username;
-        this.password=password;
-       
+class User {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  getPassword() {
+    // get the password where every character is replaced by "*"
+    return this.password.replace(/./g, "*");
+  }
+
+  setPassword(password) {
+    // validate the password before setting
+    this.password = password;
+    if (
+      password.length < 8 ||
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(password) ||
+      [...uppercaseArr].includes(password) ||
+      (password.length < 8 &&
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(password) &&
+        [...uppercaseArr].includes(password))
+    ) {
+      return "Password must be at least 8 characters long and contain atleast one number and one uppercase letter";
+    } else {
+      return password;
     }
-
-    getPassword(){
-        // get the password where every character is replaced by "*"
-     return this.password.replace(/./g,"*")
-
-    }
-
-    setPassword(password){
-        // validate the password before setting
-        this.password=password
-        if(password.length<8 ||([1,2,3,4,5,6,7,8,9,0]).includes(password)||([...uppercaseArr]).includes(password)||(password.length<8 ||([1,2,3,4,5,6,7,8,9,0]).includes(password)||([...uppercaseArr]).includes(password))){
-            return "Password must be at least 8 characters long and contain atleast one number and one uppercase letter"
-        }else{
-            return password
-        }
-    }
+  }
 }
 
 // new User object
-const person=new User("12erd","ertdda123456");
+const person = new User("12erd", "ertdda123456");
 console.log(person.getPassword()); // ************
-console.log(person.setPassword("Emrtdda12")) // Emrtdda12
+console.log(person.setPassword("Emrtdda12")); // Emrtdda12
 console.log(person.getPassword()); // *********
