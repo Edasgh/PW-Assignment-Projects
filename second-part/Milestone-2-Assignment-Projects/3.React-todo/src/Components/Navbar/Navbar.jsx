30b7decc0e4c85770624c6aeef817fe04cfa2117
Edasgh <edas25564@gmail.com>
2023-08-14 18:47:04 +0530

    "Navbar code changed in case of responsive"

--- second-part/Milestone-2-Assignment-Projects/3.React-todo/src/Components/Navbar/Navbar.jsx
+++ second-part/Milestone-2-Assignment-Projects/3.React-todo/src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import "./Navbar.css"
-import AddTaskBtn from '../Elements/AddTaskBtn/AddTaskBtn'
-
-
+import React from "react";
+import "./Navbar.css";
+import AddTaskBtn from "../Elements/AddTaskBtn/AddTaskBtn";
 
 const Navbar = () => {
   return (
-   <>
-   <nav className="navbar">
-    <ul className="nav-ul" type="none">
-      <h2><a href="/">myTodos</a></h2>
-      <li><a href="/">Home</a></li>
-      <li><a href="/todos">Todos</a></li>
-      
-    </ul>
-    <ul className="other-ul">
+    <>
+      <nav className="navbar">
+        <ul className="nav-ul" type="none">
+          <p className="nav-logo nav-logo-inside">
+            <a href="/">myTodos</a>
+          </p>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/todos">Todos</a>
+          </li>
+        </ul>
+        <ul className="other-ul">
+          <AddTaskBtn btnCls="add-task" />
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-   <AddTaskBtn btnCls="add-task" />
-
-   {/* <i className="fa-solid fa-bars"></i> */}
-    </ul>
-   {/* <i className="fa-regular fa-square-caret-down"></i> */}
-
-   </nav>
-   {/* <AddTodoModal/> */}
-     {/* class names will be used as props in add-todo component */}
-   </>
-  )
-}
-
-export default Navbar
+export default Navbar;
