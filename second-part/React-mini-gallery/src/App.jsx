1e1233c07ce5039bd7d48506017f6f0e04d9eaa0
Edasgh <edas25564@gmail.com>
2024-01-07 10:04:04 +0530

    "Starting React gallery"

---
+++ second-part/React-mini-gallery/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <p className="lkg">hi, there</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
